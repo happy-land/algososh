3bc8a485c8ef32ca824eea5a2db3d2bee99b3c59
Alena Ismagilova <ismagilovaalena90@yandex.ru>
2023-01-09 23:05:25 +0300

    fixed visit in app

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,6 +1,6 @@
 describe("app is available", () => {
   it("app should be available on localhost:3000", () => {
     cy.viewport(1400, 700);
-    cy.visit("http://localhost:3000");
+    cy.visit("/");
   });
 });
